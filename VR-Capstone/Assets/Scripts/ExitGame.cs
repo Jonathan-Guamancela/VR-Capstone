af4f4d409df4832238ce29fd89e74103c1481186
Oldsch0ol1 <RyanBrown12@my.unt.edu>
2025-03-16 21:39:36 -0500

    exit is working now, main menu camera is bugged now its inverted

--- VR-Capstone/Assets/Scripts/ExitGame.cs
+++ VR-Capstone/Assets/Scripts/ExitGame.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class ExitGame : MonoBehaviour
 {
-    public void ExitApplication()
+    public void Exit()
     {
-        // Quits the application
+        Debug.Log("Exit button clicked!");
+
+        // This will quit the application when running a build.
         Application.Quit();
+
+        // If running in the Unity Editor, stop play mode.
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
